a09cd01e15902573834db219cdcf74f17251f730
kunugibaru <r.od@me.com>
2022-11-14 23:05:20 +0900

    font!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { onMounted, Ref, ref } from 'vue'
-import * as THREE from 'three'
+import { onMounted, Ref, ref } from "vue"
+import * as THREE from "three"
+
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js"
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js"
 
 const CANV = ref<HTMLDivElement | undefined>()
 
@@ -8,64 +11,105 @@ const vec = (x: number, y: number, z: number) => new THREE.Vector3(x, y, z)
 
 const init = () => {
   const [width, height] = [600, 400]
-  
-  const camera = new THREE.PerspectiveCamera(70, width / height, 1, 5000 )
-  //const camera = new THREE.OrthographicCamera(-width, width, -height, height)
+
+  //const camera = new THREE.PerspectiveCamera(70, width / height, 1, 5000 )
+  const camera = new THREE.OrthographicCamera(-width, width, -height, height)
   //camera.position.set(0, 10, 0)
-  
 
   const scene = new THREE.Scene()
-  
+
   {
-    const light = new THREE.DirectionalLight( 0xffffff );
-    light.lookAt(1, 0, 1)
-    scene.add(light);
+    const light = new THREE.DirectionalLight(0xffffff)
+    //light.lookAt(1, 0, 1)
+    scene.add(light)
     scene.add(new THREE.AmbientLight(0xffffff, 0.5))
   }
 
   {
     const mesh = new THREE.Mesh(
-      new THREE.BoxGeometry(10, 10, 10),
-      new THREE.MeshStandardMaterial({color: new THREE.Color(1, 0, 0)})
-      )
+      new THREE.PlaneGeometry(40, 40),
+      new THREE.MeshStandardMaterial({ color: new THREE.Color(1, 0, 0) })
+    )
+    mesh.position.x = 20
     scene.add(mesh)
   }
 
-  
   const renderer = new THREE.WebGLRenderer()
+
+  let isMouseDown = false
+  renderer.domElement.onmousedown = ev => {
+    console.log(343)
+    isMouseDown = true
+  }
+
+  renderer.domElement.onmouseup = ev => {
+    console.log()
+    isMouseDown = false
+  }
+  renderer.domElement.onmouseleave = ev => {
+    isMouseDown = false
+  }
+
+  renderer.domElement.onmousemove = ev => {
+    const {movementX, movementY} = ev
+    if (isMouseDown) {
+      camera.position.x += movementX
+       camera.position.y += movementY
+    }
+
+  }
+
   renderer.setSize(width, height)
   //renderer.setPixelRatio(height / width)
   return {
-    renderer, camera, scene
+    renderer,
+    camera,
+    scene,
+    isMouseDown,
   }
 }
 
 onMounted(() => {
-  const {renderer, camera, scene} = init()
+  const { renderer, camera, scene } = init()
 
   CANV.value!.appendChild(renderer.domElement)
 
+  const loader = new FontLoader()
+
+  loader.load("helvetiker_regular.typeface.json", function (font: any) {
+    const geometry = new TextGeometry("Hello three.jsreereereereereereereeree!", {
+      font: font,
+      size: 50,
+      height: 5,
+      curveSegments: 5,
+    })
+
+    scene.add(new THREE.Mesh(geometry, new THREE.MeshStandardMaterial()))
+  })
+
   let f = 0
+
+  camera.position.set(0, 0, 10)
+  camera.up.set(0, 1, 0)
+  
+    camera.lookAt(0, 0, 0)
   const tick = () => {
-    const FF = f* 0.02
-    
+    const FF = f * 0.02
+
     camera.updateProjectionMatrix()
     renderer.render(scene, camera)
     requestAnimationFrame(tick)
+    //camera.rotation.x =  180* 0.0174533
     
-    camera.position.set(Math.cos(FF) * 10, Math.sin(FF) * 10, Math.sin(FF)* 10)
-    camera.lookAt(0, 0, 0)
 
     f++
   }
 
   tick()
 })
-
 </script>
 
 <template>
-
   <div ref="CANV"></div>
   d
 </template>
