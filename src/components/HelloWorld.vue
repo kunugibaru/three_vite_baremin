76bca0817885766ba3018685e6f01048fbf3bd76
kunugibaru <r.od@me.com>
2022-11-14 21:55:58 +0900

    bare min!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,40 @@
 <script setup lang="ts">
 import { onMounted, Ref, ref } from 'vue'
 import * as THREE from 'three'
-import { Camera } from 'three';
 
-const CANV = ref<HTMLCanvasElement | undefined>()
+const CANV = ref<HTMLDivElement | undefined>()
 
 const vec = (x: number, y: number, z: number) => new THREE.Vector3(x, y, z)
 
 const init = () => {
-  const camera = new THREE.OrthographicCamera(3,3,2,2, 0,10000)
-  camera.position.set(0, 10, 0)
-  camera.lookAt(0, 0, 0)
+  const [width, height] = [600, 400]
+  
+  const camera = new THREE.PerspectiveCamera(70, width / height, 1, 5000 )
+  //const camera = new THREE.OrthographicCamera(-width, width, -height, height)
+  //camera.position.set(0, 10, 0)
+  
 
   const scene = new THREE.Scene()
-  scene.add(new THREE.PointLight())
-  scene.add(new THREE.Mesh(
-    new THREE.SphereGeometry(1,1,1),
-    new THREE.MeshStandardMaterial()
-  ))
+  
+  {
+    const light = new THREE.DirectionalLight( 0xffffff );
+    light.lookAt(1, 0, 1)
+    scene.add(light);
+    scene.add(new THREE.AmbientLight(0xffffff, 0.5))
+  }
 
-  const [width, height] = [600, 400]
-  const renderer = new THREE.WebGLRenderer({antialias: false})
+  {
+    const mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(10, 10, 10),
+      new THREE.MeshStandardMaterial({color: new THREE.Color(1, 0, 0)})
+      )
+    scene.add(mesh)
+  }
+
+  
+  const renderer = new THREE.WebGLRenderer()
   renderer.setSize(width, height)
-  renderer.setPixelRatio(width /  height)
+  //renderer.setPixelRatio(height / width)
   return {
     renderer, camera, scene
   }
@@ -32,12 +44,19 @@ onMounted(() => {
   const {renderer, camera, scene} = init()
 
   CANV.value!.appendChild(renderer.domElement)
-  
 
+  let f = 0
   const tick = () => {
+    const FF = f* 0.02
+    
+    camera.updateProjectionMatrix()
     renderer.render(scene, camera)
     requestAnimationFrame(tick)
     
+    camera.position.set(Math.cos(FF) * 10, Math.sin(FF) * 10, Math.sin(FF)* 10)
+    camera.lookAt(0, 0, 0)
+
+    f++
   }
 
   tick()
@@ -47,8 +66,8 @@ onMounted(() => {
 
 <template>
 
-  <canvas ref="CANV"> </canvas>
-  
+  <div ref="CANV"></div>
+  d
 </template>
 
 <style scoped>
