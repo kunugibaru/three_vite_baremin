5c89758e278fe384985863163d47e7493a9b82fc
kunugibaru <r.od@me.com>
2022-11-14 21:15:32 +0900

    three

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,54 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, Ref, ref } from 'vue'
+import * as THREE from 'three'
+import { Camera } from 'three';
 
-defineProps<{ msg: string }>()
+const CANV = ref<HTMLCanvasElement | undefined>()
 
-const count = ref(0)
+const vec = (x: number, y: number, z: number) => new THREE.Vector3(x, y, z)
+
+const init = () => {
+  const camera = new THREE.OrthographicCamera(3,3,2,2, 0,10000)
+  camera.position.set(0, 10, 0)
+  camera.lookAt(0, 0, 0)
+
+  const scene = new THREE.Scene()
+  scene.add(new THREE.PointLight())
+  scene.add(new THREE.Mesh(
+    new THREE.SphereGeometry(1,1,1),
+    new THREE.MeshStandardMaterial()
+  ))
+
+  const [width, height] = [600, 400]
+  const renderer = new THREE.WebGLRenderer({antialias: false})
+  renderer.setSize(width, height)
+  renderer.setPixelRatio(width /  height)
+  return {
+    renderer, camera, scene
+  }
+}
+
+onMounted(() => {
+  const {renderer, camera, scene} = init()
+
+  CANV.value!.appendChild(renderer.domElement)
+  
+
+  const tick = () => {
+    renderer.render(scene, camera)
+    requestAnimationFrame(tick)
+    
+  }
+
+  tick()
+})
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <canvas ref="CANV"> </canvas>
+  
 </template>
 
 <style scoped>
